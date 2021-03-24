7d79b0a9fd592a543d3e7335378ca4fd872ce4f9
MaximeLeBerre <70652404+MaximeLeBerre@users.noreply.github.com>
2021-03-24 11:45:14 +0100

    put data with form in ItemDetail done

--- src/components/ItemDetails.jsx
+++ src/components/ItemDetails.jsx
@@ -1,17 +1,64 @@
 import { useParams } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Formik, Field, Form } from 'formik';
 
 export default function ItemDetails() {
   const { id } = useParams();
 
+  const [item, setItem] = useState([]);
+
+  useEffect(() => {
+    axios.get(`http://localhost:3000/api/object/${id}`).then((res) => {
+      const data = [res.data];
+      setItem(data);
+    });
+  }, [id]);
+
   return (
     <>
-      <h1>
-        Detail de item
-      </h1>
-      <h1>
-        c est id :
-        {id}
-      </h1>
+      {item.map((obj) => (
+        <div>
+          <Formik
+            initialValues={{
+              name: obj.name,
+              type: obj.type,
+              price: obj.price,
+              rating: obj.rating,
+              warranty_years: obj.warranty_years,
+              available: obj.available
+            }}
+            onSubmit={async (values) => {
+              await new Promise((r) => setTimeout(r, 500));
+              /* const res = (JSON.stringify(values, null, 2)); */
+              axios.put(`http://localhost:3000/api/object/${id}`, values)
+                .then((reponse) => reponse);
+            }}
+          >
+            <Form>
+              <label htmlFor="Name">Name</label>
+              <Field id="name" name="name" />
+
+              <label htmlFor="Type">Type</label>
+              <Field id="type" name="type" />
+
+              <label htmlFor="price">price</label>
+              <Field id="price" name="price" />
+
+              <label htmlFor="rating">rating</label>
+              <Field id="rating" name="rating" type="number" />
+
+              <label htmlFor="warranty_years">warranty_years</label>
+              <Field id="warranty_years" name="warranty_years" />
+
+              <label htmlFor="available">available</label>
+              <Field id="available" name="available" />
+
+              <button type="submit">Submit</button>
+            </Form>
+          </Formik>
+        </div>
+      ))}
     </>
   );
 }
